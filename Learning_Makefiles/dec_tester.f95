1888bcd1c2fdd45be931a5234fea0983fdd30d88
Bob <r.Caddy@pitt.edu>
2019-06-07 15:57:47 -0400

    Added some stuff to help learn about makefiles

---
+++ Learning_Makefiles/dec_tester.f95
@@ -0,0 +1,8 @@
+PROGRAM dec_tester
+  USE declaration_module
+  IMPLICIT NONE !requires me to declare all variables
+
+  write(unit=*, fmt=*) testv
+
+
+END PROGRAM dec_tester
---
+++ Learning_Makefiles/declaration_module.f95
@@ -0,0 +1,9 @@
+MODULE declaration_module
+  !USE types, ONLY: dp, pi
+  IMPLICIT NONE
+
+
+  real :: testv=7.
+
+
+END MODULE declaration_module
