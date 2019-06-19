41291b17b94c62ece4c53c8f5d6e6696b4750f23
Bob <r.Caddy@pitt.edu>
2019-06-18 20:18:34 -0400

    Kept working on exercises 17.1-17.3.  The merge subroutine is fucked, it doesn't work

--- Chapter_17_Coarrays/test_sort.f90
+++ Chapter_17_Coarrays/test_sort.f90
@@ -1,4 +1,6 @@
 PROGRAM test_sort
+  ! NOTE:  Something about the merge_sub subroutine is fucked.  This program doesn't work and that is why. All the coarray shit 
+  ! works great
   !   Purpose:
   !       Sorting arrays in parallel using coarray fortran
   !       Exercise: 17.1-17.3
@@ -6,32 +8,63 @@ PROGRAM test_sort
   !   Written June 18th, 2019.  Revised June 18th, 2019
 
   USE merge_module, ONLY: merge_sub
-  USE sort_module, ONLY: sort
+  USE sort_module,  ONLY: sort
   IMPLICIT NONE !requires me to declare all variables
 
   ! Parameters
-  INTEGER,PARAMETER :: N_SAMPLES = 100000
+  INTEGER,PARAMETER :: N_SAMPLES = 100  ! Total number of samples
 
   ! Declare Variables
-  REAL,DIMENSION(N_SAMPLES)   :: a   ! Input values to sort
-  REAL,DIMENSION(N_SAMPLES/2) :: b[*]! Coarray for parallel sorting
-  REAL,DIMENSION(N_SAMPLES)   :: out ! Sorted output values
-  INTEGER                     :: i   ! Loop index
-  ! INTEGER                     :: m   ! Number of values to sort
+  REAL,DIMENSION(N_SAMPLES)      :: a         ! Input values to sort
+  REAL,DIMENSION(:), ALLOCATABLE :: b[:]      ! Coarray for parallel sorting
+  REAL,DIMENSION(:), ALLOCATABLE :: bfinal[:] ! sorting array for last image incase N_SAMPLES/NUM_IMAGES() has a remainder
+  REAL,DIMENSION(N_SAMPLES)      :: out       ! Sorted output values
+  REAL,DIMENSION(N_SAMPLES)      :: input    ! Sorted output values
+  INTEGER                        :: i         ! Loop index
+  INTEGER                        :: t1, t2, t3! temp variables
+  INTEGER                        :: err       ! error code variable
+  INTEGER                        :: length    ! length of each coarray b
+
 
   ! ===========================================================================
-  ! Now sort using 2 images
+  ! Now sort using n images
   ! ===========================================================================
+
+
+  ! ===== First Allocate all the Arrays =======================================
+  length = N_SAMPLES/NUM_IMAGES() ! length of each coarray
+
+  ! this just goes through and allocates the coarrays in all the images
+  ALLOCATE(b(length)[*], stat=err)
+  IF (err /= 0) WRITE(unit=*, fmt=*) ": Allocation request denied for coarray"
+  SYNC ALL ! make sure to sync everybody
+
+
+  t1 = N_SAMPLES-NUM_IMAGES()*length
+  ALLOCATE(bfinal(t1)[*], stat=err)
+  IF (err /= 0) WRITE(unit=*, fmt=*) ": Allocation request denied for bfinal"
+  SYNC ALL
+
 
   ! ===== Create the input array using Image 1 ================================
-  IF ( this_image() == 1 ) THEN
-    ! Allocate the data to sort
+  IF ( THIS_IMAGE() == 1 ) THEN
+    ! create the data to sort
     CALL RANDOM_NUMBER(a)
 
-    ! Copy the data into the working arrays ! for each image.
-    b(:)[1] = a(1:N_SAMPLES/2)
-    b(:)[2] = a(N_SAMPLES/2+1:N_SAMPLES)
+    ! Copy the data into the working arrays for each image.
+    t1 = 1      ! the variable for the lower limit of each array slice
+    t2 = length ! the variable for the upper limit of each array slice
+    DO i = 1, NUM_IMAGES()-1
+      ! this takes consecutive slices of length 'length' and assigns them
+      ! to each image.  To handle numbers that don't divide evenly the last
+      ! image gets all the extra elements in the array
+      b(:)[i] = a(t1:t2) ! assignment
+      t1 = t2 + 1      ! set new lower bound
+      t2 = t2 + length ! set new upper bound
+    END DO
 
+    ! copy data to the last image
+    bfinal(:)[NUM_IMAGES()] = a(t1:)
   END IF
 
   !*************************************************
@@ -44,7 +77,11 @@ PROGRAM test_sort
   ! Now all images can run in parallel.
   ! Sort the data in each image
   !*************************************************
-  CALL sort( b, N_SAMPLES/2 )
+  IF (THIS_IMAGE() /= NUM_IMAGES()) THEN
+    CALL sort(b, length)
+  ELSE
+    CALL sort(bfinal, t1)
+  END IF
 
   !*************************************************
   ! Wait until all images are finished
@@ -55,9 +92,27 @@ PROGRAM test_sort
   ! Now merge the data back into a common output
   ! array using image 1, and display the results.
   !*************************************************
-  IF ( this_image() == 1 ) THEN
+  IF ( THIS_IMAGE() == 1 ) THEN
+    input(:length) = b(:)[1]
+
     ! Merge the data
-    CALL merge_sub ( b(:)[1], N_SAMPLES/2, b(:)[2], N_SAMPLES/2, out, N_SAMPLES )
+    DO i = 2, NUM_IMAGES()
+      t2 = (i-1)*length
+      t3 = SIZE(input(:(i-1)*length))+SIZE(b(:)[i])
+
+      IF (i /= NUM_IMAGES()) THEN
+        CALL merge_sub (input(:t2), t2, b(:)[i], SIZE(b(:)[i]), out(:t3), t3)
+        write(unit=*, fmt=*) input
+        write(unit=*, fmt=*) out
+        input(:) = out(:)
+        out(:) = 0
+
+      ELSE
+        CALL merge_sub (input(:t2), t2, bfinal(:)[i], SIZE(bfinal(:)[i]), out(:), N_SAMPLES)
+      END IF
+
+    END DO
+
 
     ! Display first 5 samples
     WRITE (*,'(A)') 'First 5 samples:'
